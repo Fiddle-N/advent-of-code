83c413683dd7a93e5d1a5daa6c571583af3b2a7c
Fadil Nohur <fadil.nohur@outlook.com>
2020-12-21 20:23:12 +0000

    AoC Day 21

---
+++ day_21/test.py
@@ -0,0 +1,18 @@
+from day_21 import process
+
+
+def test():
+    food_str = """\
+mxmxvkd kfcds sqjhc nhms (contains dairy, fish)
+trh fvjkl sbzzf mxmxvkd (contains dairy)
+sqjhc fvjkl (contains soy)
+sqjhc mxmxvkd sbzzf (contains fish)"""
+    allegen_assessment = process.AllergenAssessment.from_string(food_str)
+    assert allegen_assessment.non_allergenic_ingredients == {'kfcds', 'nhms', 'sbzzf', 'trh'}
+    assert allegen_assessment.non_allergenic_ingredient_occurrence() == 5
+    assert allegen_assessment.allergenic_ingredients == {
+        'dairy': 'mxmxvkd',
+        'fish': 'sqjhc',
+        'soy': 'fvjkl',
+    }
+    assert allegen_assessment.canonical_dangerous_ingredients == 'mxmxvkd,sqjhc,fvjkl'
