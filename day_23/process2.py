e482911ef28e43c530b77126c17456adf51216db
Fadil Nohur <fadil.nohur@outlook.com>
2021-05-03 15:56:04 +0100

    Add list based implementation of linked list

--- day_23/process2.py
+++ day_23/process2.py
@@ -4,18 +4,24 @@ import more_itertools
 
 class CrabCups:
 
-    def __init__(self, input, number_of_cups=1_000_000, moves=10_000_000):
+    def __init__(self, input, number_of_cups=1_000_000, moves=10_000_000, data_type='list'):
         initial_cup_list = [int(x) for x in input]
+        self.first_cup = initial_cup_list[0]
         initial_max_cups = max(initial_cup_list)
         if number_of_cups is not None:
             cup_list = initial_cup_list + list(range(initial_max_cups + 1, number_of_cups + 1))
         else:
+            number_of_cups = initial_max_cups + 1
             cup_list = initial_cup_list
         self.max_cups = max(cup_list)
-        self.cups = {}
+        self.data_type = data_type
+        if self.data_type == 'dict':
+            self.cups = {}
+        else:
+            self.cups = [None] * (number_of_cups + 1)
         for cup, next_cup in more_itertools.windowed(cup_list, 2):
             self.cups[cup] = next_cup
-        self.cups[cup_list[-1]] = cup_list[0]       # tie up last cup and first cup
+        self.cups[next_cup] = cup_list[0]  # tie up last cup and first cup
         self.moves = moves
 
     @staticmethod
@@ -38,7 +44,7 @@ class CrabCups:
 
     def process(self):
         cups = self.cups.copy()
-        current = next(iter(cups))
+        current = self.first_cup
         for move in range(self.moves):
             removed = self._to_be_removed(cups, current)
             destination = self._get_destination_cup(current, removed)
