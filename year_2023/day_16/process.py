48a8c338447d37ed4ace015b3e9151eae218806f
Fadil Nohur <fadil.nohur@outlook.com>
2023-12-16 21:22:06 +0000

    Year 2023 Day 16

---
+++ year_2023/day_16/process.py
@@ -0,0 +1,186 @@
+import collections
+import dataclasses
+import enum
+from typing import Self
+
+
+@dataclasses.dataclass(frozen=True)
+class Coords:
+    x: int
+    y: int
+
+    def __add__(self, other: Self) -> Self:
+        return Coords(self.x + other.x, self.y + other.y)
+
+
+class Direction(enum.Enum):
+    UPWARDS = enum.auto()
+    RIGHTWARDS = enum.auto()
+    DOWNWARDS = enum.auto()
+    LEFTWARDS = enum.auto()
+
+
+OFFSET_COORDS = {
+    Direction.UPWARDS: Coords(0, -1),
+    Direction.RIGHTWARDS: Coords(1, 0),
+    Direction.DOWNWARDS: Coords(0, 1),
+    Direction.LEFTWARDS: Coords(-1, 0),
+}
+
+
+@dataclasses.dataclass(frozen=True)
+class BeamState:
+    location: Coords
+    direction: Direction
+
+
+class ContraptionSpace(enum.Enum):
+    EMPTY_SPACE = '.'
+    FORWARDS_DIAGONAL_MIRROR = '/'
+    BACKWARDS_DIAGONAL_MIRROR = '\\'
+    VERTICAL_SPLITTER = '|'
+    HORIZONTAL_SPLITTER = '-'
+
+
+FORWARDS_DIAGONAL_DIRECTIONS = {
+    Direction.RIGHTWARDS: Direction.UPWARDS,
+    Direction.UPWARDS: Direction.RIGHTWARDS,
+    Direction.LEFTWARDS: Direction.DOWNWARDS,
+    Direction.DOWNWARDS: Direction.LEFTWARDS,
+}
+
+BACKWARDS_DIAGONAL_DIRECTIONS = {
+    Direction.RIGHTWARDS: Direction.DOWNWARDS,
+    Direction.DOWNWARDS: Direction.RIGHTWARDS,
+    Direction.LEFTWARDS: Direction.UPWARDS,
+    Direction.UPWARDS: Direction.LEFTWARDS,
+}
+
+
+class Contraption:
+
+    def __init__(self, contraption_input):
+        self.contraption = {}
+        contraption_rows = contraption_input.splitlines()
+        self.width = len(contraption_rows[0])
+        self.height = len(contraption_rows)
+        for y, row in enumerate(contraption_rows):
+            for x, space in enumerate(row):
+                self.contraption[Coords(x, y)] = ContraptionSpace(space)
+
+    @classmethod
+    def read_file(cls):
+        with open("input.txt") as f:
+            return cls(f.read().strip())
+
+    def _is_valid_coord(self, coord):
+        return (0 <= coord.x < self.width) and (0 <= coord.y < self.height)
+
+    def simulate_beam(self, init_location=Coords(-1, 0), init_direction=Direction.RIGHTWARDS):
+        # init locations do not exist on the grid
+        # but is used to model where the beam is originating from
+        init_beam_state = BeamState(
+            location=init_location, direction=init_direction
+        )
+        energised_spaces: dict[Coords, set[Direction]] = collections.defaultdict(set)
+        beam_states = collections.deque([init_beam_state])
+        while beam_states:
+            beam_state = beam_states.pop()
+            direction = beam_state.direction
+            location = beam_state.location + OFFSET_COORDS[direction]
+            if not self._is_valid_coord(location):
+                continue
+            space = self.contraption[location]
+
+            match (space, direction):
+                case (
+                    (ContraptionSpace.EMPTY_SPACE, _)
+                    | (ContraptionSpace.HORIZONTAL_SPLITTER, (Direction.LEFTWARDS | Direction.RIGHTWARDS))
+                    | (ContraptionSpace.VERTICAL_SPLITTER, (Direction.UPWARDS | Direction.DOWNWARDS))
+                ):
+                    # beam passes through as normal
+                    next_beam_states = [
+                        BeamState(location=location, direction=direction)
+                    ]
+                case ContraptionSpace.HORIZONTAL_SPLITTER, (Direction.UPWARDS | Direction.DOWNWARDS):
+                    next_beam_states = [
+                        BeamState(location=location, direction=next_direction)
+                        for next_direction in (Direction.LEFTWARDS, Direction.RIGHTWARDS)
+                    ]
+                case ContraptionSpace.VERTICAL_SPLITTER, (Direction.LEFTWARDS | Direction.RIGHTWARDS):
+                    next_beam_states = [
+                        BeamState(location=location, direction=next_direction)
+                        for next_direction in (Direction.UPWARDS, Direction.DOWNWARDS)
+                    ]
+                case ContraptionSpace.FORWARDS_DIAGONAL_MIRROR, _:
+                    next_beam_states = [
+                        BeamState(location=location, direction=FORWARDS_DIAGONAL_DIRECTIONS[direction])
+                    ]
+                case ContraptionSpace.BACKWARDS_DIAGONAL_MIRROR, _:
+                    next_beam_states = [
+                        BeamState(location=location, direction=BACKWARDS_DIAGONAL_DIRECTIONS[direction])
+                    ]
+                case _:
+                    raise ValueError('Unhandled space/direction case')
+
+            for next_beam_state in next_beam_states:
+                if next_beam_state.direction in energised_spaces[location]:
+                    # we've been here before
+                    continue
+                energised_spaces[location].add(next_beam_state.direction)
+                beam_states.appendleft(next_beam_state)
+        return energised_spaces
+
+    def best_configuration(self):
+        left_starting_positions = [
+            {'init_location': Coords(-1, y), 'init_direction': Direction.RIGHTWARDS}
+            for y in range(self.height)
+        ]
+        right_starting_positions = [
+            {'init_location': Coords(self.width, y), 'init_direction': Direction.LEFTWARDS}
+            for y in range(self.height)
+        ]
+        top_starting_positions = [
+            {'init_location': Coords(x, -1), 'init_direction': Direction.DOWNWARDS}
+            for x in range(self.width)
+        ]
+        bottom_starting_positions = [
+            {'init_location': Coords(x, self.height), 'init_direction': Direction.UPWARDS}
+            for x in range(self.width)
+        ]
+        starting_positions = (
+            left_starting_positions
+            + right_starting_positions
+            + top_starting_positions
+            + bottom_starting_positions
+        )
+        results = []
+        for start_params in starting_positions:
+            result = self.simulate_beam(**start_params)
+            tile_num = len(result)
+            results.append((result, tile_num))
+
+        best_result = max(results, key=lambda result_detail: result_detail[1])
+        return best_result
+
+
+def main() -> None:
+    contraption = Contraption.read_file()
+    top_left_energised_spaces = contraption.simulate_beam()
+    top_left_tile_num = len(top_left_energised_spaces)
+    print(
+        "Number of tiles ending up being energised with beam entering from top-left:",
+        top_left_tile_num
+    )
+    best_config = contraption.best_configuration()
+    _, best_tile_num = best_config
+    print(
+        "Number of tiles ending up being energised with beam entering from any edge:",
+        best_tile_num
+    )
+
+
+if __name__ == "__main__":
+    import timeit
+
+    print(timeit.timeit(main, number=1))
---
+++ year_2023/day_16/test_process.py
@@ -0,0 +1,139 @@
+from year_2023.day_16 import process
+
+
+def test_contraption_beam_from_top_left_corner():
+    contraption_input = (
+r""".|...\....
+|.-.\.....
+.....|-...
+........|.
+..........
+.........\
+..../.\\..
+.-.-/..|..
+.|....-|.\
+..//.|...."""
+    )
+    contraption = process.Contraption(contraption_input)
+    energised_spaces = contraption.simulate_beam()
+    assert energised_spaces == {
+        process.Coords(0, 0): {process.Direction.RIGHTWARDS},
+        process.Coords(1, 0): {process.Direction.UPWARDS, process.Direction.DOWNWARDS},
+        process.Coords(2, 0): {process.Direction.LEFTWARDS},
+        process.Coords(3, 0): {process.Direction.LEFTWARDS},
+        process.Coords(4, 0): {process.Direction.LEFTWARDS},
+        process.Coords(5, 0): {process.Direction.LEFTWARDS},
+        process.Coords(1, 1): {process.Direction.DOWNWARDS},
+        process.Coords(5, 1): {process.Direction.UPWARDS},
+        process.Coords(1, 2): {process.Direction.DOWNWARDS},
+        process.Coords(5, 2): {process.Direction.UPWARDS, process.Direction.DOWNWARDS},
+        process.Coords(6, 2): {process.Direction.LEFTWARDS, process.Direction.RIGHTWARDS},
+        process.Coords(7, 2): {process.Direction.RIGHTWARDS},
+        process.Coords(8, 2): {process.Direction.RIGHTWARDS},
+        process.Coords(9, 2): {process.Direction.RIGHTWARDS},
+        process.Coords(1, 3): {process.Direction.DOWNWARDS},
+        process.Coords(5, 3): {process.Direction.DOWNWARDS},
+        process.Coords(6, 3): {process.Direction.UPWARDS},
+        process.Coords(1, 4): {process.Direction.DOWNWARDS},
+        process.Coords(5, 4): {process.Direction.DOWNWARDS},
+        process.Coords(6, 4): {process.Direction.UPWARDS},
+        process.Coords(1, 5): {process.Direction.DOWNWARDS},
+        process.Coords(5, 5): {process.Direction.DOWNWARDS},
+        process.Coords(6, 5): {process.Direction.UPWARDS},
+        process.Coords(1, 6): {process.Direction.DOWNWARDS},
+        process.Coords(4, 6): {process.Direction.RIGHTWARDS},
+        process.Coords(5, 6): {process.Direction.DOWNWARDS, process.Direction.RIGHTWARDS},
+        process.Coords(6, 6): {process.Direction.UPWARDS, process.Direction.DOWNWARDS},
+        process.Coords(7, 6): {process.Direction.LEFTWARDS},
+        process.Coords(0, 7): {process.Direction.LEFTWARDS},
+        process.Coords(1, 7): {process.Direction.LEFTWARDS, process.Direction.RIGHTWARDS},
+        process.Coords(2, 7): {process.Direction.RIGHTWARDS},
+        process.Coords(3, 7): {process.Direction.RIGHTWARDS},
+        process.Coords(4, 7): {process.Direction.UPWARDS},
+        process.Coords(5, 7): {process.Direction.DOWNWARDS},
+        process.Coords(6, 7): {process.Direction.DOWNWARDS},
+        process.Coords(7, 7): {process.Direction.UPWARDS},
+        process.Coords(1, 8): {process.Direction.UPWARDS, process.Direction.DOWNWARDS},
+        process.Coords(2, 8): {process.Direction.LEFTWARDS},
+        process.Coords(3, 8): {process.Direction.LEFTWARDS},
+        process.Coords(4, 8): {process.Direction.LEFTWARDS},
+        process.Coords(5, 8): {process.Direction.LEFTWARDS, process.Direction.DOWNWARDS},
+        process.Coords(6, 8): {process.Direction.LEFTWARDS, process.Direction.RIGHTWARDS},
+        process.Coords(7, 8): {process.Direction.UPWARDS, process.Direction.DOWNWARDS},
+        process.Coords(1, 9): {process.Direction.DOWNWARDS},
+        process.Coords(5, 9): {process.Direction.DOWNWARDS},
+        process.Coords(7, 9): {process.Direction.DOWNWARDS},
+    }
+    assert len(energised_spaces) == 46
+
+
+def test_contraption_best_energised_spaces():
+    contraption_input = (
+r""".|...\....
+|.-.\.....
+.....|-...
+........|.
+..........
+.........\
+..../.\\..
+.-.-/..|..
+.|....-|.\
+..//.|...."""
+    )
+    contraption = process.Contraption(contraption_input)
+    best_config = contraption.best_configuration()
+    best_energised_spaces, best_tile_num = best_config
+    assert best_energised_spaces == {
+        process.Coords(1, 0): {process.Direction.UPWARDS, process.Direction.DOWNWARDS},
+        process.Coords(2, 0): {process.Direction.LEFTWARDS},
+        process.Coords(3, 0): {process.Direction.LEFTWARDS, process.Direction.DOWNWARDS},
+        process.Coords(4, 0): {process.Direction.LEFTWARDS},
+        process.Coords(5, 0): {process.Direction.LEFTWARDS},
+        process.Coords(1, 1): {process.Direction.DOWNWARDS},
+        process.Coords(3, 1): {process.Direction.DOWNWARDS},
+        process.Coords(5, 1): {process.Direction.UPWARDS},
+        process.Coords(1, 2): {process.Direction.DOWNWARDS},
+        process.Coords(3, 2): {process.Direction.DOWNWARDS},
+        process.Coords(5, 2): {process.Direction.UPWARDS, process.Direction.DOWNWARDS},
+        process.Coords(6, 2): {process.Direction.LEFTWARDS, process.Direction.RIGHTWARDS},
+        process.Coords(7, 2): {process.Direction.RIGHTWARDS},
+        process.Coords(8, 2): {process.Direction.RIGHTWARDS},
+        process.Coords(9, 2): {process.Direction.RIGHTWARDS},
+        process.Coords(1, 3): {process.Direction.DOWNWARDS},
+        process.Coords(3, 3): {process.Direction.DOWNWARDS},
+        process.Coords(5, 3): {process.Direction.DOWNWARDS},
+        process.Coords(6, 3): {process.Direction.UPWARDS},
+        process.Coords(1, 4): {process.Direction.DOWNWARDS},
+        process.Coords(3, 4): {process.Direction.DOWNWARDS},
+        process.Coords(5, 4): {process.Direction.DOWNWARDS},
+        process.Coords(6, 4): {process.Direction.UPWARDS},
+        process.Coords(1, 5): {process.Direction.DOWNWARDS},
+        process.Coords(3, 5): {process.Direction.DOWNWARDS},
+        process.Coords(5, 5): {process.Direction.DOWNWARDS},
+        process.Coords(6, 5): {process.Direction.UPWARDS},
+        process.Coords(1, 6): {process.Direction.DOWNWARDS},
+        process.Coords(3, 6): {process.Direction.DOWNWARDS},
+        process.Coords(4, 6): {process.Direction.RIGHTWARDS},
+        process.Coords(5, 6): {process.Direction.RIGHTWARDS, process.Direction.DOWNWARDS},
+        process.Coords(6, 6): {process.Direction.UPWARDS, process.Direction.DOWNWARDS},
+        process.Coords(7, 6): {process.Direction.LEFTWARDS},
+        process.Coords(0, 7): {process.Direction.LEFTWARDS},
+        process.Coords(1, 7): {process.Direction.LEFTWARDS, process.Direction.RIGHTWARDS},
+        process.Coords(2, 7): {process.Direction.LEFTWARDS, process.Direction.RIGHTWARDS},
+        process.Coords(3, 7): {process.Direction.LEFTWARDS, process.Direction.RIGHTWARDS},
+        process.Coords(4, 7): {process.Direction.UPWARDS},
+        process.Coords(5, 7): {process.Direction.DOWNWARDS},
+        process.Coords(6, 7): {process.Direction.DOWNWARDS},
+        process.Coords(7, 7): {process.Direction.UPWARDS},
+        process.Coords(1, 8): {process.Direction.UPWARDS, process.Direction.DOWNWARDS},
+        process.Coords(2, 8): {process.Direction.LEFTWARDS},
+        process.Coords(3, 8): {process.Direction.LEFTWARDS},
+        process.Coords(4, 8): {process.Direction.LEFTWARDS},
+        process.Coords(5, 8): {process.Direction.LEFTWARDS, process.Direction.DOWNWARDS},
+        process.Coords(6, 8): {process.Direction.LEFTWARDS, process.Direction.RIGHTWARDS},
+        process.Coords(7, 8): {process.Direction.UPWARDS, process.Direction.DOWNWARDS},
+        process.Coords(1, 9): {process.Direction.DOWNWARDS},
+        process.Coords(5, 9): {process.Direction.DOWNWARDS},
+        process.Coords(7, 9): {process.Direction.DOWNWARDS},
+    }
+    assert best_tile_num == 51
