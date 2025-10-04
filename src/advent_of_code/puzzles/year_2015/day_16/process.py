bad03d068fdec89c609f5ab3dc97f45676952ef0
Fadil Nohur <fadil.nohur@outlook.com>
2025-10-04 16:14:06 +0100

    2015 Day 16
    - Solution

---
+++ src/advent_of_code/puzzles/year_2015/day_16/process.py
@@ -0,0 +1,73 @@
+import re
+import itertools
+from collections.abc import Callable
+
+from advent_of_code.common import read_file, timed_run
+
+
+SUE_PATTERN = r"Sue \w+: (?P<compound_1_name>\w+): (?P<compound_1_val>\d+), (?P<compound_2_name>\w+): (?P<compound_2_val>\d+), (?P<compound_3_name>\w+): (?P<compound_3_val>\d+)"
+
+MFCSAM_RESULT_PART_1 = {
+    "children": lambda val: val == 3,
+    "cats": lambda val: val == 7,
+    "samoyeds": lambda val: val == 2,
+    "pomeranians": lambda val: val == 3,
+    "akitas": lambda val: val == 0,
+    "vizslas": lambda val: val == 0,
+    "goldfish": lambda val: val == 5,
+    "trees": lambda val: val == 3,
+    "cars": lambda val: val == 2,
+    "perfumes": lambda val: val == 1,
+}
+
+MFCSAM_RESULT_PART_2 = {
+    "children": lambda val: val == 3,
+    "cats": lambda val: val > 7,
+    "samoyeds": lambda val: val == 2,
+    "pomeranians": lambda val: val < 3,
+    "akitas": lambda val: val == 0,
+    "vizslas": lambda val: val == 0,
+    "goldfish": lambda val: val < 5,
+    "trees": lambda val: val > 3,
+    "cars": lambda val: val == 2,
+    "perfumes": lambda val: val == 1,
+}
+
+
+def parse_sue_details(sue_details_text: str) -> list[dict[str, int]]:
+    sue_details: list[dict[str, int]] = []
+    for line in sue_details_text.splitlines():
+        if (match := re.fullmatch(SUE_PATTERN, line)) is not None:
+            sue_detail = {
+                match.group("compound_1_name"): int(match.group("compound_1_val")),
+                match.group("compound_2_name"): int(match.group("compound_2_val")),
+                match.group("compound_3_name"): int(match.group("compound_3_val")),
+            }
+            sue_details.append(sue_detail)
+        else:
+            raise ValueError("did not match re pattern")
+    return sue_details
+
+
+def find_sue(
+    sue_details: list[dict[str, int]], mfcsam_result: dict[str, Callable[[int], bool]]
+) -> int:
+    for sue_no, sue_detail in zip(itertools.count(start=1), sue_details):
+        if all(mfcsam_result[name](val) for name, val in sue_detail.items()):
+            return sue_no
+    raise ValueError("Error - could not find correct Sue")
+
+
+def run():
+    sue_details_text = read_file()
+    sue_details = parse_sue_details(sue_details_text)
+    print(find_sue(sue_details, mfcsam_result=MFCSAM_RESULT_PART_1))
+    print(find_sue(sue_details, mfcsam_result=MFCSAM_RESULT_PART_2))
+
+
+def main() -> None:
+    timed_run(run)
+
+
+if __name__ == "__main__":
+    main()
