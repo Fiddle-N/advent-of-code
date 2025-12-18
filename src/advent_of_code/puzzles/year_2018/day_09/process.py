dbf1db50e04efd5e93df74ba76c8e1a7fbcaa9c3
Fadil Nohur <fadil.nohur@outlook.com>
2025-12-18 22:26:07 +0000

    2018 Day 9
    - Rename optimised solution

--- src/advent_of_code/puzzles/year_2018/day_09/process.py
+++ src/advent_of_code/puzzles/year_2018/day_09/process.py
@@ -3,7 +3,6 @@ import dataclasses
 import itertools
 import re
 import timeit
-import typing
 
 
 @dataclasses.dataclass
@@ -19,7 +18,6 @@ class Elf:
 
 
 class MarbleGame:
-
     def __init__(self, elves, last_marble):
         self.circle = collections.deque()
         self.elves = [Elf(elf_no) for elf_no in range(elves)]
@@ -27,21 +25,23 @@ class MarbleGame:
 
     @classmethod
     def from_string(cls, string, marble_multiplier=1):
-        if match := re.fullmatch(r'(\d+) players; last marble is worth (\d+) points', string):
+        if match := re.fullmatch(
+            r"(\d+) players; last marble is worth (\d+) points", string
+        ):
             return cls(int(match.group(1)), int(match.group(2)) * marble_multiplier)
         else:
             raise Exception
 
     @classmethod
     def read_file(cls, marble_multiplier=1):
-        with open('input.txt') as f:
+        with open("input.txt") as f:
             return cls.from_string(f.read().rstrip(), marble_multiplier)
 
     def play(self):
         for marble, elf in enumerate(itertools.cycle(self.elves)):
             if marble and (marble % 23 == 0):
                 self.circle.rotate(7)
-                elf.score += (self.circle.pop() + marble)
+                elf.score += self.circle.pop() + marble
                 self.circle.rotate(-1)
             else:
                 self.circle.rotate(-1)
@@ -55,8 +55,10 @@ def main():
     print(f"Winning elf's score: {marble_game.play()}")
 
     marble_game_2 = MarbleGame.read_file(marble_multiplier=100)
-    print(f"Winning elf's score with last marble 100 times larger: {marble_game_2.play()}")
+    print(
+        f"Winning elf's score with last marble 100 times larger: {marble_game_2.play()}"
+    )
 
 
-if __name__ == '__main__':
-    print(f'Completed in {timeit.timeit(main, number=1)} seconds')
+if __name__ == "__main__":
+    print(f"Completed in {timeit.timeit(main, number=1)} seconds")
