5c568b5e8bc3e05aa719ec9a1bc427c70421eba8
Fadil Nohur <fadil.nohur@outlook.com>
2026-07-25 11:02:32 +0100

    Update merge intervals to merge adjacent intervals

--- src/advent_of_code/common/intervals.py
+++ src/advent_of_code/common/intervals.py
@@ -1,12 +1,17 @@
 __all__ = ["merge_intervals"]
 
 
-def merge_intervals(intervals: list[tuple[int, int]]) -> list[tuple[int, int]]:
+def merge_intervals(
+    intervals: list[tuple[int, int]], integer_vals: bool = True
+) -> list[tuple[int, int]]:
     intervals = sorted(intervals)
     left, right = intervals[:1], intervals[1:]
     for next_i in right:
         last_i = left.pop()
-        if last_i[0] <= next_i[0] <= last_i[1]:
+        if (last_i[0] <= next_i[0] <= last_i[1]) or (
+            integer_vals and (next_i[0] == last_i[1] + 1)
+        ):
+            # merge overlapping or adjacent intervals
             i = (last_i[0], max(last_i[1], next_i[1]))
             left.append(i)
         else:
