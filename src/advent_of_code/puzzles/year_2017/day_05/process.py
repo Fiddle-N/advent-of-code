51b7d7d2817ea54bd76b784f0a82da9b415dc78c
Fadil Nohur <fadil.nohur@outlook.com>
2026-08-01 09:27:50 +0100

    2017 Day 5
    - Amend to not mutate input

--- src/advent_of_code/puzzles/year_2017/day_05/process.py
+++ src/advent_of_code/puzzles/year_2017/day_05/process.py
@@ -3,7 +3,10 @@ from typing import Literal
 from advent_of_code.common import read_file, timed_run, parse_ints
 
 
-def execute_instrs(offsets: list[int], mode: Literal["part_1", "part_2"]) -> int:
+def execute_instrs(
+    offsets: list[int], mode: Literal["part_1", "part_2"]
+) -> tuple[list[int], int]:
+    offsets = offsets.copy()
     idx = 0
     steps = 0
     while True:
@@ -14,15 +17,18 @@ def execute_instrs(offsets: list[int], mode: Literal["part_1", "part_2"]) -> int
         steps += 1
 
         if idx < 0 or idx >= len(offsets):
-            return steps
+            return (offsets, steps)
 
 
 def run():
     raw_instrs = read_file()
     instrs = parse_ints(raw_instrs)
 
-    print(execute_instrs(instrs.copy(), mode="part_1"))
-    print(execute_instrs(instrs.copy(), mode="part_2"))
+    _, steps_p1 = execute_instrs(instrs, mode="part_1")
+    print(steps_p1)
+
+    _, steps_p2 = execute_instrs(instrs, mode="part_2")
+    print(steps_p2)
 
 
 def main() -> None:
--- src/advent_of_code/puzzles/year_2017/day_05/test_process.py
+++ src/advent_of_code/puzzles/year_2017/day_05/test_process.py
@@ -3,13 +3,13 @@ from advent_of_code.puzzles.year_2017.day_05 import process
 
 def test_run_part_1() -> None:
     instrs = [0, 3, 0, 1, -3]
-    steps = process.execute_instrs(instrs, mode="part_1")
-    assert instrs == [2, 5, 0, 1, -2]
+    final_instrs, steps = process.execute_instrs(instrs, mode="part_1")
+    assert final_instrs == [2, 5, 0, 1, -2]
     assert steps == 5
 
 
 def test_run_part_2() -> None:
     instrs = [0, 3, 0, 1, -3]
-    steps = process.execute_instrs(instrs, mode="part_2")
-    assert instrs == [2, 3, 2, 3, -1]
+    final_instrs, steps = process.execute_instrs(instrs, mode="part_2")
+    assert final_instrs == [2, 3, 2, 3, -1]
     assert steps == 10
