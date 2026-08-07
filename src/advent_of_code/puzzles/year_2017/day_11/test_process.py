21a1ef5e929ceec786dbd784cc5b722518e731c2
Fadil Nohur <fadil.nohur@outlook.com>
2026-08-07 22:56:08 +0100

    2017 Day 11
    - Rename test method

--- src/advent_of_code/puzzles/year_2017/day_11/test_process.py
+++ src/advent_of_code/puzzles/year_2017/day_11/test_process.py
@@ -12,7 +12,7 @@ from advent_of_code.puzzles.year_2017.day_11 import process
         ("se,sw,se,sw,sw", 3),
     ],
 )
-def test_calculate_knot_hash(dir_text: str, exp_steps) -> None:
+def test_calculate_fewest_steps(dir_text: str, exp_steps) -> None:
     dirs = process.parse_dirs(dir_text)
     steps, _ = process.calculate_fewest_steps(dirs)
     assert steps == exp_steps
